f201fc15edeb3c4113c9e10c15fb2985cc8b1606
pm-dhairyashil-ghatage <dhairyashil.ghatage@pubmatic.com>
2024-07-31 16:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import subprocess, os
 
+
+# Constants
+script_path = f"{os.getcwd()}/interact.expect"
+index_dict = {"Prebid.js": 2,2:"Prebid.js","OW prebid Fork": 3,3:"OW prebid Fork","LLM Data": 7}
+
+# Define default values for the inputs
+default_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidjFfNjk0NF8xMzExODYwXzI1MV9XZWQgSnVsIDMxIDA2OjU0OjQxIFVUQyAyMDI0In0.j2tsyo_zUn5JRiygcebhcJtGJP7uq61BKM2E9ARU4Vw"
+default_mode = "ADVANCED"
+default_question = "in one paragraph, how does publisher allowlist inline upload api work?"
+
+
 # Set the page configuration - title and layout mode
 st.set_page_config(page_title="PubMatic Code MindMaster", layout="wide")
 
@@ -48,16 +59,9 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.title("Prebid Wiki")
 st.write("This is a Advanced tool to answer your queries about Prebid Code")
 
-# Define default values for the inputs
-default_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidjFfNjk0NF8xMzExODYwXzI1MV9XZWQgSnVsIDMxIDA2OjU0OjQxIFVUQyAyMDI0In0.j2tsyo_zUn5JRiygcebhcJtGJP7uq61BKM2E9ARU4Vw"
-default_mode = "BASIC"
-index_dict = {"Prebid.js": 2,"OW prebid Fork": 3,"LLM Data": 7}
-default_question = "in one paragraph, how does publisher allowlist inline upload api work?"
-
-
 # Create multiselect field for project index
-index = ["Prebid.js", "OW prebid Fork", "LLM Data"]
-index_response = st.selectbox("Select a project", index, placeholder="Choose an option", disabled=False, label_visibility="visible")
+index = index_dict.keys()
+index_response = st.multiselect("Select a project", index, placeholder="Choose an option", disabled=False, label_visibility="visible")
 
 suggested_questions_list = ["Fetch release notes for OW as per version", "Fetch release notes for OW as per date",
                             "Fetch release notes for Prebid as per version",
@@ -76,16 +80,13 @@ question = question + " in my code"
 
 print(question)
 
-selected_value_for_cmd = str(index_dict.get(index_response, 6))  # Default value if no checkbox is selected
-output_file_name = str(selected_value_for_cmd) + "final_response.txt"
+# selected_value_for_cmd = str(index_dict.get(index_response, 6))  # Default value if no checkbox is selected
+selected_value_for_cmd = [index_dict[project] for project in index_response]
 
+# output_file_name = str(selected_value_for_cmd) + "final_response.txt"
+
+output_file_name = "final_response.txt"
 output_file = f"output/{output_file_name}"
-
-
-# Absolute path to the script
-script_path = f"{os.getcwd()}/interact.expect"
-
-print(script_path)
 
 # Button to trigger the script
 if st.button('Get Info'):
@@ -99,34 +100,40 @@ if st.button('Get Info'):
     # os.chdir('/home/dhirajdarakhe/Documents/fantastic6')
     
     # Construct the command to run the script using the absolute path
-    cmd = [script_path, default_auth_key, default_mode,selected_value_for_cmd, question]
-
-    print("Current working directory before subprocess call:", os.getcwd())
-
-    # Execute the command and capture the output
-    result = subprocess.run(cmd, capture_output=True, text=True)
     
+    for index in selected_value_for_cmd:
+        cmd = [script_path, default_auth_key, default_mode,str(index), question]
+        print("Current working directory before subprocess call:", os.getcwd())
+        # Execute the command and capture the output
+        result = subprocess.run(cmd, capture_output=True, text=True)
 
-    # Replace the temporary message with the actual content
-    placeholder.empty()
 
-    # Check for errors in script execution
-    if result.stderr:
-        st.error(f"Error: {result.stderr}")
-    # else:
-    #     # Get the numeric value corresponding to the selected key from index_dict
-    #     numeric_value = index_dict[selected_keys[0]]
+        # Replace the temporary message with the actual content
+        placeholder.empty()
+        if os.path.exists(output_file):
+            with open(output_file, 'r') as file:
+                output_contents = file.read()
 
-    #     # Assuming output_file_name is the original file name that includes the key
-    #     # For demonstration, let's say it's something like "heimdall_final_response.txt"
+            question= question+output_contents+f"refering this from {index_dict.get(int(index))}.+respond in 200 words only"
+        
 
-    #     # Replace the selected key in the file name with its numeric value from index_dict
-    #     modified_file_name = output_file_name.replace(str(selected_keys[0]), str(numeric_value))
+        # Check for errors in script execution
+        if result.stderr:
+            st.error(f"Error: {result.stderr}")
+        # else:
+        #     # Get the numeric value corresponding to the selected key from index_dict
+        #     numeric_value = index_dict[selected_keys[0]]
 
-    #     # Construct the full path to the file
-    #     output_file = f"/home/dhirajdarakhe/Documents/fantastic6/{modified_file_name}"
+        #     # Assuming output_file_name is the original file name that includes the key
+        #     # For demonstration, let's say it's something like "heimdall_final_response.txt"
 
-        # If script ran successfully, read and display the contents of the output file
+        #     # Replace the selected key in the file name with its numeric value from index_dict
+        #     modified_file_name = output_file_name.replace(str(selected_keys[0]), str(numeric_value))
+
+        #     # Construct the full path to the file
+        #     output_file = f"/home/dhirajdarakhe/Documents/fantastic6/{modified_file_name}"
+
+            # If script ran successfully, read and display the contents of the output file
     if os.path.exists(output_file):
         print(output_file)
         with open(output_file, 'r') as file:
